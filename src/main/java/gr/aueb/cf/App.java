bc018be99b00c6ddeff93ea058ae65d5d702e1be
DimDrk <dimdrk@gmail.com>
2024-09-20 22:50:52 +0300

    initial connection with database

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,13 +1,38 @@
 package gr.aueb.cf;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import gr.aueb.cf.model.Branch;
+import gr.aueb.cf.model.BranchCategory;
+import gr.aueb.cf.model.Teacher;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("school1PU");
+        EntityManager em = emf.createEntityManager();
+
+
+        Teacher androutsos = new Teacher(null, "Αθανάσιος", "Ανδρούτσος", true, null, null, null);
+        BranchCategory pe = new BranchCategory(null, "ΠΕ", null, null);
+        Branch pe80 = new Branch(null, "ΠΕ80", pe, null);
+
+        pe80.addTeacher(androutsos);
+        pe.addTeacher(androutsos);
+
+        em.getTransaction().begin();
+
+        em.persist(androutsos);
+        em.persist(pe);
+        em.persist(pe80);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+
     }
 }
